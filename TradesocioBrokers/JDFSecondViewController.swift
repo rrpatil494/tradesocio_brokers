141d29bf0596c98d647c744f8f640416e0b219e9
Rohan Patil <rohanpatil@tradesocio.com>
2019-01-25 17:12:49 +0530

    rohan : V1.1 added one file

---
+++ TradesocioBrokers/JDFSecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  JDFSecondViewController.swift
+//  TradesocioBrokers
+//
+//  Created by TradeSocio on 25/01/19.
+//  Copyright © 2019 Tradesocio. All rights reserved.
+//
+
+
+// Added file in JFD Broker V1.1
+
+
+import UIKit
+
+class JDFSecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
